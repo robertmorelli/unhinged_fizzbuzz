177a81e00f49ec0af24b3332b326c1c165baf5be
Robert Morelli <robertondino@outlook.com>
2026-01-22 09:10:15 -0700

    shorten brine

--- brine.py
+++ brine.py
@@ -1,19 +1,9 @@
-i = 1
-LIMIT = input()
 template = """
 i = {i}
 LIMIT = {LIMIT}
 template = {template!r}
 if i <= LIMIT:
-    if i % 15 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-
+    print(["{i}", "Fizz", "Buzz", "FizzBuzz"][(i % 3 == 0) + 2 * (i % 5 == 0)].format(i=i))
     exec(template.format(i=i+1, LIMIT=LIMIT, template=template))
 """
-exec(template.format(i=i, LIMIT=LIMIT, template=template))
+exec(template.format(i=1, LIMIT=input(), template=template))
