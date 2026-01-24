dc7d79f3e2da6cb318cf21e525db87c9adcb816d
Robert Morelli <robertondino@outlook.com>
2026-01-24 15:56:16 -0700

    revert to simple branchless loop

--- branchless.asm
+++ branchless.asm
@@ -1,156 +1,36 @@
+# the branchless loop to base this on. this is not done
+
 .text
 .globl main
 main:
-	li 	$v0, 	5
-	syscall
-        move 	$t3,	$v0
-	la	$t0,	loop_label
-	srl	$t0,	$t0,	2
-	lw	$t1,	mask26
-	and	$t0,	$t0,	$t1
-	lw	$t2,	j_opcode
-	or	$t3,	$t3,	$t2
-loop_label:
-        li	$a0,	next
-        lw	$a0,	0($a0)
-        jr	$a0
-continue_loop:
-	subu	$t3,	$t3,	1
-        and	$t2,	$t2,	$t3
-	or	$t4,	$t2,	$t0
-
-	la	$t5,	jump_or_syscall
-	sw	$t4,	0($t5)
-	li	$v0,	10
-jump_or_syscall:
-	nop
-	syscall
-
-j_opcode:	.word	0x08000000
-mask26:		.word	0x03FFFFFF
-fizz:		.asciz "fizz\n"
-buzz:		.asciz "buzz\n"
-fizzbuzz:	.asciz "fizzbuzz\n"
-next:	.word	one
-
-do_next:
-	li	$a0,	next
-        jr	$a0
-
-n_r:
-	xli	$v0,	1
-	xor	$a0,	$t3,	$t2
-	syscall
+	li 	$v0, 	5				# Load syscall code for read_int
+	syscall						# Execute syscall
+	move 	$t3,	$v0			# Move the input integer from $v0 to $t0
+	la	$t0,	loop_label 		# t0 to mask into instruction
+	srl	$t0,	$t0,	2		# move address to align
+	lw	$t1,	mask26			# j imm mask
+	and	$t0,	$t0,	$t1		# make $t0 have the right imm in the right place
+	lw	$t2,	j_opcode		# used later for masking
+	or	$t3,	$t3,	$t2		# add the j opcode to the counter
+loop_label:						# at address that just so happens to end in c
+								# print loop index
 	li	$v0,	11
 	li	$a0,	10
 	syscall
-        jr $ra
+	li	$v0,	1
+	xor	$a0,	$t3,	$t2
+	syscall
 
-fizz_r:
-	li	$v0,	4
-	la	$a0, fizz
-        syscall
-        jr $ra
+	subu	$t3,	$t3,	1	# decrement counter
+	and	$t2,	$t2,	$t3		# change to opcode based on if the counter removed its top bit
+	or	$t4,	$t2,	$t0		# put the opcode and loop label together
 
-buzz_r:
-	li	$v0,	4
-	la	$a0, buzz
-        syscall
-        jr $ra
-
-fizzbuzz_r:
-	li	$v0,	4
-	la	$a0, fizzbuzz
-        syscall
-        jr $ra
-
-
-one:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), two
-        j	continue_loop
-two:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), three
-        j	continue_loop
-
-three:
-        jal	fizz_r
-        li	$a0,	next
-        sw	0($a0), four
-        j	continue_loop
-
-four:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), five
-        j	continue_loop
-
-five:
-        jal	buzz_r
-        li	$a0,	next
-        sw	0($a0), six
-        j	continue_loop
-
-six:
-        jal	fizz_r
-        li	$a0,	next
-        sw	0($a0), seven
-        j	continue_loop
-
-seven:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), eight
-        j	continue_loop
-
-eight:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), nine
-        j	continue_loop
-
-nine:
-        jal	fizz_r
-        li	$a0,	next
-        sw	0($a0), ten
-        j	continue_loop
-
-ten:
-        jal	buzz_r
-        li	$a0,	next
-        sw	0($a0), eleven
-        j	continue_loop
-
-eleven:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), twelve
-        j	continue_loop
-
-twelve:
-        jal	fizz_r
-        li	$a0,	next
-        sw	0($a0), thirteen
-        j	continue_loop
-
-thirteen:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), fourteen
-        j	continue_loop
-
-fourteen:
-        jal	n_r
-        li	$a0,	next
-        sw	0($a0), fifteen
-        j	continue_loop
-
-fifteen:
-        jal	fizzbuzz_r
-        li	$a0,	next
-        sw	0($a0), one
-        j	continue_loop
-
+	la	$t5,	jump_or_syscall
+	sw	$t4,	0($t5)			# store the instruction
+	li	$v0,	10				# load for exit syscall
+jump_or_syscall:
+	nop							# either jump to loop_label or syscall and exit
+	syscall
+.data
+	j_opcode:	.word	0x08000000
+	mask26:		.word	0x03FFFFFF
